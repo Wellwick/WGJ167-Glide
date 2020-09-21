e26f5bba4e73b575732622300a221da137bac82c
Michael Boyle <mbbexley@hotmail.co.uk>
2020-09-21 12:06:31 +0100

    Add terrain cells

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -5,105 +5,54 @@ using UnityEngine;
 public class TerrainGeneration : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 size = new Vector2(100f,100f);
-
-    [SerializeField, Range(0.001f, 2f)]
-    private float resolution = 1f;
+    private int closeCells = 5;
 
     [SerializeField]
-    private float minHeight, maxHeight;
+    public Vector2 size = new Vector2(100f,100f);
+
+    [SerializeField, Range(0.05f, 2f)]
+    public float resolution = 1f;
 
     [SerializeField]
-    private int noiseOffsetSeed = 12345;
+    public float minHeight, maxHeight;
+
+    [SerializeField]
+    public int noiseOffsetSeed = 12345;
 
     [SerializeField, Range(0.001f, 20f)]
-    private float zoom = 1.0f;
+    public float zoom = 1.0f;
 
-    public int vertexCount = 0;
-
-    private MeshRenderer meshRenderer;
-    private MeshFilter meshFilter;
-    private Mesh mesh;
+    [SerializeField]
+    private Transform player;
     
-    private bool requestCalculate;
+    public bool requestCalculate;
 
-    private void PrepareMesh() {
-        meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
-
-        meshFilter = gameObject.AddComponent<MeshFilter>();
-        
-    }
+    private List<TerrainCell> cells;
 
     // Start is called before the first frame update
     void Start() {
-        PrepareMesh();
-        CalculateMesh();
-
+        cells = new List<TerrainCell>();
+        AddCell(Vector2.zero);
+        AddCell(new Vector2(10f,0f));
         requestCalculate = false;
     }
 
-    private void CalculateMesh() {
-        mesh = new Mesh();
-        // Amount of vertices is equal to the width * depth / resolution
-        int width = (int)((size.x + resolution) / resolution);
-        int depth = (int)((size.y + resolution) / resolution);
+    private void AddCell(Vector2 position) {
+        GameObject newObject = Instantiate(new GameObject("Cell"), new Vector3(position.x, 0f, position.y), Quaternion.identity, transform);
+        TerrainCell tc = newObject.AddComponent<TerrainCell>();
+        tc.PrepareTerrainCell(this);
+        cells.Add(tc);
+    }
 
+    private void RemoveCell(TerrainCell cell) {
+        cells.Remove(cell);
+        Destroy(cell);
+    }
 
-        Random.InitState(noiseOffsetSeed);
-        Vector2 offset = Vector2.one;
-        offset.x *= Random.value * 1000000;
-        offset.y *= Random.value * 1000000;
-
-        Vector3[] vertices = new Vector3[width * depth];
-        int i = 0;
-        for (float z=0f; z <= size.y; z += resolution) {
-            for (float x = 0f; x <= size.x; x += resolution, i++) {
-                float y = Mathf.Lerp(minHeight, maxHeight, Mathf.PerlinNoise((x/zoom) + offset.x, (z/zoom) + offset.y));
-                vertices[i] = new Vector3(x, y, z);
-                //vertices[i] = new Vector3(x, 0f, z);
-            }
+    private void CalculateAllMeshes() {
+        foreach (TerrainCell tc in cells) {
+            tc.CalculateMesh();
         }
-        mesh.vertices = vertices;
-        vertexCount = vertices.Length;
-
-        // For each 'quad' there is 2 triangles, or 6 triangle points
-        int[] tris = new int[width * depth * 6];
-
-        i = 0;
-        for (int z = 0; z < depth-1; z++, i++) {
-            for (int x = 0; x < width-1; x++, i++) {
-                int localIndex = i * 6;
-                tris[localIndex] = i;
-                tris[localIndex + 1] = i + width;
-                tris[localIndex + 2] = i + 1;
-
-                tris[localIndex + 3] = i + width;
-                tris[localIndex + 4] = i + width + 1;
-                tris[localIndex + 5] = i + 1;
-
-            }
-        }
-        mesh.triangles = tris;
-
-        Vector3[] normals = new Vector3[width * depth];
-        /*
-        for (i = 0; i < normals.Length; i++) {
-            // Need to figure out the average plane from the nearby triangles
-            // and then use that normal!
-            normals[i] = Vector3.up;
-        }
-        mesh.normals = normals;
-        */
-        mesh.RecalculateNormals();
-
-        Vector2[] uv = new Vector2[width * depth];
-        for (i = 0; i < uv.Length; i++) {
-            uv[i] = new Vector2(vertices[i].x, vertices[i].z);
-        }
-        mesh.uv = uv;
-
-        meshFilter.mesh = mesh;
     }
 
     private void OnValidate() {
@@ -114,7 +63,7 @@ public class TerrainGeneration : MonoBehaviour
     void Update()
     {
         if (requestCalculate) {
-            CalculateMesh();
+            CalculateAllMeshes();
             requestCalculate = false;
         }
     }
