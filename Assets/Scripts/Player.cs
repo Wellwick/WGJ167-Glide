605ae52f95e5f862493e18be58133d50e2e2f07a
Murat Özcan <murat.ozcan@bil.omu.edu.tr>
2020-09-24 17:40:52 +0300

    ./wind theme

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     [SerializeField]
     private float speed = 1f;
+
+    private uint themeID;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        themeID = AkSoundEngine.PostEvent("WindTheme", gameObject);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     private void FixedUpdate() {
--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID WINDTHEME = 2988929114U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
