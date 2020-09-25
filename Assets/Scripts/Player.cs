006421c2032f13d97f6ac98d197f7edc169411a2
Michael Boyle <mbbexley@hotmail.co.uk>
2020-09-25 17:53:17 +0100

    Make it so you can fly up and down and lose speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField]
-    private float speed = 1f;
+    private float maxSpeed = 1f;
+
+    public float speed;
+
+    private Vector3 direction;
+
+    private Transform squirrel;
+    private Camera camera;
 
     private uint themeID;
     // Start is called before the first frame update
     void Start() {
         themeID = AkSoundEngine.PostEvent("WindTheme", gameObject);
+        direction = transform.right;
+        foreach (Transform t in transform) {
+            if (t.name == "Squirrel") {
+                squirrel = t;
+            } else if (t.name == "Main Camera") {
+                camera = t.GetComponent<Camera>();
+            }
+        }
+        speed = maxSpeed;
     }
 
     // Update is called once per frame
     void Update() {
+        direction.y = Input.GetAxis("Vertical") - (Mathf.Clamp(5f - (speed-4f) * 0.5f, 0.2f, 7f) * 0.3f);
 
     }
 
     private void FixedUpdate() {
-        transform.position += new Vector3(speed * Time.deltaTime, 0f);
+        transform.position += direction.normalized * speed * Time.deltaTime;
+        Vector3 sRotation = squirrel.localEulerAngles;
+        sRotation.z = direction.y * -45f;
+        squirrel.localEulerAngles = sRotation;
+        speed = Mathf.Clamp(speed - ((direction.y+0.1f) * Time.deltaTime*3f), 0, maxSpeed);
+        float fovMultiplier = Mathf.Lerp(0.5f, 1.0f, speed / maxSpeed);
+        camera.fieldOfView = 60 * fovMultiplier;
     }
 }
